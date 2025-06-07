b4c42c8a3c2779e680c66e6823b53508a0bfa6ad
yongminkim0501 <yongmingim166@gmail.com>
2025-06-07 15:42:17 +0900

    feat: youtube api와 연결하여 spotify api의 결과를 통하여 간단하게 검색하여 확인하는 코드 구현

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from service.spotify.spotify_connect import SpotifyAuthManage
+from service.youtube.youtube_connect import ConnectYoutube
+
 if __name__ == "__main__":
-    Spotify_Object = SpotifyAuthManage()
-    search_results = Spotify_Object.search_track("아이유", limit=5)
-    #for track in search_results:
-        #print(f"🎵 {track['name']} - {track['artist']}")
-    print(search_results)
+    spotify_object = SpotifyAuthManage()
+    search_results = spotify_object.search_track("아이유", limit=5)
+    youtube_object = ConnectYoutube()
+
+    first_track = search_results[0]  # ✅ 수정됨
+    search_query = f"{first_track['artist']} {first_track['name']}"
+    print(search_query)
+
+    result = youtube_object.search_videos(search_query)
+    print(result)
+
